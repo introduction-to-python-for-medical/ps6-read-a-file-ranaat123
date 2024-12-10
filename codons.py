fabaa2f0a18981f20ba8fbb4650df411ad2ace15
ranaat123 <rana.atalla5@gmail.com>
2024-12-10 17:06:09 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,16 @@
+
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
 
+    file_path = "data/codons.txt"
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    mapping_between_codons = {}
+    
+    for row in rows:
+      cells = row.strip().split('\t')
+      codon = cells[0]
+      amino_acid = cells[2]
+      mapping_between_codons[codon] = amino_acid
 
+    return mapping_between_codons
